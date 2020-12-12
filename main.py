b21063bd7e3b9ce245ac347c8604243940345334
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-12 22:11:01 +0600

    feat: save data inside a json file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # password generator app
 
+import json
 import pyperclip
 from tkinter import *
 from tkinter import messagebox
@@ -48,18 +49,24 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    # create new dictionary for store in json file
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
     # checking empty fields
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Opps", message=" Please fill up the empty fields.")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"Is It Correct Info \n Email: {email} " f"\n Password: {password} \nIs "
-                                               f"it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password} \n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        # save the date on json file
+        with open("data.json", "a") as data_file:
+            # writing inside the json file with indent 4
+            json.dump(new_data, data_file, indent=4)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ------------ UI SETUP --------------------------------- #
