169f15af509ace3426eb8ed09b2972ce212f43a0
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-12 21:49:19 +0600

    feat: add constant values

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import pyperclip
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+
+
+# ------------ CONSTANT ------------- #
+
+GREENISH_YELLOW = "#E6EDB7"
+GREENISH_WHITE = "#FDFCEF"
+LIGHT_SKY = "#C0E5E4"
+LIGHT_GREEN = "#92E3A9"
+COURIER_FONT = "Courier"
