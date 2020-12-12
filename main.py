f16cfd406cee97e52795e2d45268173df48657d0
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-12 21:55:53 +0600

    feat: generate password funtion
    this function will generate random password in the password entry field and automatically copy password on our clipboard.

--- main.py
+++ main.py
@@ -14,6 +14,34 @@ LIGHT_GREEN = "#92E3A9"
 COURIER_FONT = "Courier"
 
 
+# ------------ PASSWORD GENERATOR ----------------------- #
+
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    # add all password in a list
+    password_list = password_letters + password_symbols + password_numbers
+    # shuffle those generated password in the list
+    shuffle(password_list)
+
+    # join password
+    password = "".join(password_list)
+    # show generated password in the password label field
+    password_entry.insert(0, password)
+    # copy password on the clipboard automatically
+    pyperclip.copy(password)
+
+
 # ------------ SAVED PASSWORD --------------------------- #
 
 def save():
